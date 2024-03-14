618ed9865376b9e0bd8492eda40121b97178bbb6
Nishan Ghimire <nishanghimire5@gmail.com>
2024-03-15 09:10:33 +1100

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+require('dotenv').config()
+const express = require('express')
+const app = express()
+
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+app.get('/login',(req, res)=>{
+res.send('<h1>Please login</h1>')
+})
+app.get('/nishan',(req,res)=>{
+  res.send('<h2>This is Nishan Ghimire<h2>')  
+})
+
+app.listen(process.env.port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
